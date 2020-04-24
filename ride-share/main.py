ee9451da8f5a0bd6b319580ae415de53271858e3
Sam Silver <sasi1851@colorado.edu>
2020-04-24 13:11:53 -0400

    updated data cleaning function

--- ride-share/main.py
+++ ride-share/main.py
@@ -30,18 +30,39 @@ import datetime as dt
 # MLP 
 
 
-def clean_data(data, col_list):
+def clean_data(data, date_col_list, thresh_dict={"driver": 4, "passenger": 4}):
     cleaned_df = data.copy()
-    cleaned_df = pd.get_dummies(data, columns=['city'], drop_first = True)
-    for col in col_list:
+    cleaned_df = pd.get_dummies(cleaned_df, columns=['city'], drop_first = True)
+
+    # convert date_col_list to datetime
+    for col in date_col_list:
         cleaned_df[col] = pd.to_datetime(churn_df[col])
-    cleaned_df['churn'] = cleaned_df['last_trip_date'] < date_30_days_ago()
+    
+    # create churn col
+    cleaned_df['churn'] = cleaned_df['last_trip_date'] < date_30_days_ago('2014-07-01')
+
+    # convert to binary
     cleaned_df['churn'].replace({True: 1, False: 0}, inplace=True)
     cleaned_df['luxury_car_user'].replace({True: 1, False: 0}, inplace=True)
+
+    # fill na
+    cleaned_df = pd.get_dummies(cleaned_df, columns=['phone'])
+
+    # ratings thresholds
+    driver_rating_thresh = thresh_dict['driver']
+    cleaned_df['high_driver_rating'] = (cleaned_df['avg_rating_of_driver'] >= driver_rating_thresh).astype(int)
+    cleaned_df['low_driver_rating'] = (cleaned_df['avg_rating_of_driver'] < driver_rating_thresh).astype(int)
+
+    passenger_rating_thresh = thresh_dict['passenger']
+    cleaned_df['high_passenger_rating'] = (cleaned_df['avg_rating_by_driver'] >= passenger_rating_thresh).astype(int)
+    cleaned_df['low_passenger_rating'] = (cleaned_df['avg_rating_by_driver'] < passenger_rating_thresh).astype(int)
+
+    cleaned_df.drop(columns=['avg_rating_of_driver', 'avg_rating_by_driver'], inplace=True)
+    
     return cleaned_df
 
-def date_30_days_ago():
-    current_date = datetime.strptime('2014-07-01', '%Y-%m-%d')
+def date_30_days_ago(date):
+    current_date = datetime.strptime(date, '%Y-%m-%d')
     earliest_date = current_date - dt.timedelta(days=30)
     return earliest_date
 
@@ -53,4 +74,3 @@ if __name__ == '__main__':
     churn_df = clean_data(churn_df, ['last_trip_date', 'signup_date'])
     churn_test_df = clean_data(churn_test_df, ['last_trip_date', 'signup_date'])
     churn_train_df = clean_data(churn_train_df, ['last_trip_date', 'signup_date'])
-    print(churn_df.head())
